289b8cab15208555945127b21f9b6ea8b7906395
Sam <24442697+digitalXmage@users.noreply.github.com>
2020-07-08 21:38:59 +0100

    Add files via upload

--- Dwm/build_dwm.sh
+++ Dwm/build_dwm.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+
+#install depedencies
+sudo apt-get install -y libx11-dev libxft-dev libxinerama-dev
+
 wget https://dl.suckless.org/dwm/dwm-6.2.tar.gz
 
 tar xzvf dwm-6.2.tar.gz
 
+rm *.gz
+
 cd dwm-6.2
 
-for patch in ../patches/*.diff; do patch -p1 < "$patch"; done
+
+sed -i 's,FREETYPEINC = ${X11INC}/freetype2,#FREETYPEINC = ${X11INC}/freetype2,g' config.mk
+sudo make install
+
+
+mv dwm /usr/bin
+
+
+echo "dwm" >  /home/sam/.xinitrc
+
+#for patch in ../patches/*.diff; do patch -p1 < "$patch"; done
+
 
