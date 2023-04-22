da8f23e64e04d8725594a3fd06600f1c839ff1ca
Sam D <24442697+digitalXmage@users.noreply.github.com>
2023-04-22 13:18:01 +0100

    Completed improvements
    Can now enter a string of numbers and open multiple bookmarks via terminal at the same time. Makes it more efficient in use. Also annotated document better.

--- bookmarks/bookmark.sh
+++ bookmarks/bookmark.sh
@@ -1,62 +1,34 @@
 #!/bin/bash
 
-#$bookmark_path="/home/$(whoami)/.bookmarks"
 
-#read and display links as command line arg, and then open firefox with the supplied link they have chosen, by copying the text to a variable in bash
-#and then executing browser to get to it
-
-
-#hardcoding would make it easier
-#websites=("
-
-
-# ./program -print (prints all the weblinks from the array)
-#
-# ./program -add (adds a weblink to the array)
-#
-# ./program -delete (deletes a weblink from array) 
-#
-# ./program -edit (edits an entry) given the keyname
-#
-# ./program -open (opens a website link provided the name has been given in the dictionry
-#
-#
-#Maybe it would be best to store and read from a log using awk and sed maybe, just to get the hang off it, and makes it easier to manage
-#
-#
-# Use awk to print and make it look nice, by extracpulating the name form the url
-#
-
-#setup function if the option given, then we must init a graph install manager
-#and prompt the user for the path to the bookmarks, and bash stores that location
-#in an environment variable maybe, or something with persistence
-#or hardcode it somehowi
-#
-#user will need to change the path to the bookmarks.txt within this bash script itself
-#run once to create the .bookmarks file in the home directory
-setup()
-{
-	touch /home/$(whoami)/.bookmarks
-	echo "bookmarks file created /home/user/.bookmarks"
-}
-
-
+#Help screen
 print_help()
 {
 	printf "bookmarks p\t\t\t(Print bookmarks)\nbookmarks o bookmark_number\t(Open bookmark)\nbookmarks e\t\t\t(edit bookmarks)\n"
 }
 
+#read and write the contents of bookmarks.txt in a certain format to stdout
 print_bookmarks()
 {
 	awk -F '/' '{printf("%d \t%s\n",NR,$3)}' ~/scripts/bash/bookmarks.txt
 }
 
 
+#open chosen bookmarks using chrome browser
 open_bookmark()
 {
-	google-chrome $(awk -F '/' -v input=$1 'input~NR {print $0}' ~/scripts/bash/bookmarks.txt) & disown	
+	for byte in $1
+	do
+		#check byte is a number
+		pattern='^[0-9]+$'
+		if [[ $byte=~pattern ]];
+		then
+			google-chrome $(awk -F '/' -v input=$1 'input~NR {print $0}' ~/scripts/bash/bookmarks.txt) & disown
+		fi
+	done	
 }
 
+#Main logic resides here
 logic()
 {
 	#help page if no args supplied,only need to test 1st arg to come to conclusion
@@ -73,10 +45,10 @@ logic()
 		print_bookmarks
 	#open link	
 	elif [[ $1 == 'o' ]]; then
-		req='^[0-9]+$'
-		if [[ $2 =~ $req ]] ; then
+	#	req='^[0-9]+$'
+	#	if [[ $2 =~ $req ]] ; then
 			open_bookmark $2
-		fi
+		#fi
 	fi
 	
 	
@@ -84,12 +56,5 @@ logic()
 
 }
 
+#initial stack frame
 logic $1 $2
-
-#logic resides here
-
-#Printing bookmarks
-#print_bookmarks
-
-#pattern matching
-#open_bookmark $1 
